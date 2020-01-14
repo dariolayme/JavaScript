2b816e858ac7a01d49add8db3b0083c45ad3f3ab
Dario Escr <dariolayme@gmail.com>
2020-01-14 18:22:34 -0300

    Curso JS

---
+++ Ejemplo01/app.js
@@ -0,0 +1,13 @@
+
+//Con esto se agrega ala pagina un titulo mediante JS.
+//document.write("<h1> HOllllllllla </h1>");
+
+//Muestra un mensaje en la consola web
+console.log('adsdad');
+
+var objeto = {
+    color: "black",
+    size: 12
+}
+
+document.write(objeto.color + ' '+ objeto.size);
